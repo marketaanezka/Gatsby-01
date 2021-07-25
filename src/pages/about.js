1c0eb76da475d0debaf6676909d70fcffd410d42
Marketa Krucka <marketakrucka@gmail.com>
2021-07-25 21:50:30 +0200

    added plugins

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,12 @@
-// Step 1: Import your component
-import * as React from 'react'
+import React from 'react'
 import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About">
-      <p>About pagggee</p>
+      <h2>About me</h2>
     </Layout>
   )
 }
-// Step 3: Export your component
+
 export default AboutPage
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle="Blog">
+      <p>We will see blog posts here</p>
+    </Layout>
+  )
+}
+// Step 3: Export your component
+export default BlogPage
