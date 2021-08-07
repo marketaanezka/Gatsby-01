29200a77af6dacd1a40d24eaa605eac52c76ef9e
Marketa Krucka <marketakrucka@gmail.com>
2021-08-07 18:26:41 +0200

    listing blog titles on blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,31 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby 01",
+    siteUrl: `https://www.yourdomain.tld`,
+    title: `Gatsby 01`,
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-catch-links",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-styled-components",
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     `gatsby-transformer-remark`,
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "pages",
+        name: `pages`,
         path: `${__dirname}/src/pages`,
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "images",
+        name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: `gatsby-plugin-react-svg`,
       options: {
         rule: {
           include: /assets/
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,43 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { graphql } from "gatsby"
+import { Link } from 'gatsby'
 
-const BlogPage = () => {
+const BlogPage = ({data}) => {
   return (
     <Layout>
       <p>We will see blog posts here</p>
+      {data.allMarkdownRemark.edges.map(post => (
+        <div key={post.node.id}>
+          <h3>{post.node.frontmatter.title}</h3>
+          <small>Author: {post.node.frontmatter.author} </small>
+          <small>Date: {post.node.frontmatter.date} </small>
+          <Link to={post.node.frontmatter.path}>
+            Open blogpost
+          </Link>
+          <hr />
+        </div>
+      ))}
     </Layout>
   )
 }
-// Step 3: Export your component
+
+export const pageQuery = graphql `
+  query BlogIndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            author
+            path
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`
+
 export default BlogPage
